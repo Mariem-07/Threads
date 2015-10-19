458151c0dd69b1774ce8cc998812344b4d71aaa5
Mariem SOMRANI <somranimariem8@gmail.com>
2015-10-19 00:08:45 +0000

    Create SecondActivity.java

---
+++ SecondActivity.java
@@ -0,0 +1,15 @@
+package tn.insat.iac.thread;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecondActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.welcome);
+	}
+
+}
